a7b7668eac9853fd36f3d7153ea45001152fe388
Park Ju Hyung <qkrwngud825@gmail.com>
2015-10-05 14:12:14 +0900

    build_clean: copy defconfig to .config
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_clean.sh
+++ build_clean.sh
@@ -5,3 +5,5 @@ make mrproper -j16 -i
 git reset --hard
 git ls-files . --ignored --exclude-standard --others --directory | while read file; do echo $file; rm -rf $file; done
 git ls-files . --exclude-standard --others --directory | while read file; do echo $file; rm -rf $file; done
+
+cp defconfig .config

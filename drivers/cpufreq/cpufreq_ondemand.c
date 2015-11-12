83de0a12da728c55771f01362b8631ecd621562c
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2015-11-12 20:24:28 +0100

    bfs: Put changes made by BFS behind CONFIG_SCHED_BFS
    Try to get a system built without BFS to behave the same. #ifdefs
    are not added where the added code is a no-op anyway.

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -20,7 +20,12 @@
 #include "cpufreq_ondemand.h"
 
 /* On-demand governor macros */
+#ifdef CONFIG_SCHED_BFS
 #define DEF_FREQUENCY_UP_THRESHOLD		(63)
+#else
+#define DEF_FREQUENCY_UP_THRESHOLD		(80)
+#endif
+
 #define DEF_SAMPLING_DOWN_FACTOR		(1)
 #define MAX_SAMPLING_DOWN_FACTOR		(100000)
 #define MICRO_FREQUENCY_UP_THRESHOLD		(95)
@@ -134,7 +139,7 @@ static void dbs_freq_increase(struct cpufreq_policy *policy, unsigned int freq)
 }
 
 /*
- * Every sampling_rate, we check, if current idle time is less than 37%
+ * Every sampling_rate, we check, if current idle time is less than 20%
  * (default), then we try to increase frequency. Else, we adjust the frequency
  * proportional to load.
  */

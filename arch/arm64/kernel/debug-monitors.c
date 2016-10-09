ea234919399776f1c348748ac1523105aa7b13b5
Park Ju Hyung <qkrwngud825@gmail.com>
2016-10-10 05:34:05 +0900

    Revert "Squash apply stable-queue/queue-4.8@e7e4d749d57a8a383d5e7f37b2bce9a2e03127cd"
    This reverts commit a79d174038cc3071cf70cfa52a6c051abf1427f3.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- arch/arm64/kernel/debug-monitors.c
+++ arch/arm64/kernel/debug-monitors.c
@@ -435,10 +435,8 @@ NOKPROBE_SYMBOL(kernel_active_single_step);
 /* ptrace API */
 void user_enable_single_step(struct task_struct *task)
 {
-	struct thread_info *ti = task_thread_info(task);
-
-	if (!test_and_set_ti_thread_flag(ti, TIF_SINGLESTEP))
-		set_regs_spsr_ss(task_pt_regs(task));
+	set_ti_thread_flag(task_thread_info(task), TIF_SINGLESTEP);
+	set_regs_spsr_ss(task_pt_regs(task));
 }
 NOKPROBE_SYMBOL(user_enable_single_step);
 

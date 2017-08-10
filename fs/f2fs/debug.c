20f710034cbed76ae81da883e78ae1f1b3665e7d
Park Ju Hyung <qkrwngud825@gmail.com>
2017-08-10 16:52:28 +0900

    f2fs: add power source awareness for background GC
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- fs/f2fs/debug.c
+++ fs/f2fs/debug.c
@@ -326,8 +326,13 @@ static int stat_show(struct seq_file *s, void *v)
 			   si->prefree_count, si->free_segs, si->free_secs);
 		seq_printf(s, "CP calls: %d (BG: %d)\n",
 				si->cp_count, si->bg_cp_count);
-		seq_printf(s, "GC calls: %d (BG: %d)\n",
+		seq_printf(s, "GC calls: %d (BG: %d)",
 			   si->call_count, si->bg_gc);
+#ifdef CONFIG_F2FS_POWER_AWARE
+		if (power_supply_is_system_supplied() == 0)
+			seq_puts(s, " (No external power source - BG disabled)");
+#endif
+		seq_puts(s, "\n");
 		seq_printf(s, "  - data segments : %d (%d)\n",
 				si->data_segs, si->bg_data_segs);
 		seq_printf(s, "  - node segments : %d (%d)\n",

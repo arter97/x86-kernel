4113be95e77e56d14e28f6d0a5f1def44f32679b
Steven Barrett <steven@liquorix.net>
2018-07-14 13:14:40 -0500

    ZEN: Implement zen-tune v4.18
    4.9:
    In a surprising turn of events, while benchmarking and testing
    hierarchical scheduling with BFQ + writeback throttling, it turns out
    that raising the number of requests in queue _actually_ improves
    responsiveness and completely eliminates the random stalls that would
    normally occur without hierarchical scheduling.
    
    To make this test more intense, I used the following test:
    
    Rotational disk1: rsync -a /source/of/data /target/to/disk1
    Rotational disk2: rsync -a /source/of/data /target/to/disk2
    
    And periodically attempted to write super fast with:
    dd if=/dev/zero of=/target/to/disk1/block bs=4096
    
    This wrote 10gb incredibly fast to writeback and I encountered zero
    stalls through this entire test of 10-15 minutes.
    
    My suspicion is that with cgroups, BFQ is more able to properly sort
    among multiple drives, reducing the chance of a starved process.  This
    plus writeback throttling completely eliminate any outstanding bugs with
    high writeback ratios, letting the user enjoy low latency writes
    (application thinks they're already done), and super high throughput due
    to batched writes in writeback.
    
    Please note however, without the following configuration, I cannot
    guarantee you will not get stalls:
    
    CONFIG_BLK_CGROUP=y
    CONFIG_CGROUP_WRITEBACK=y
    CONFIG_IOSCHED_CFQ=y
    CONFIG_CFQ_GROUP_IOSCHED=y
    CONFIG_IOSCHED_BFQ=y
    CONFIG_BFQ_GROUP_IOSCHED=y
    CONFIG_DEFAULT_BFQ=y
    CONFIG_SCSI_MQ_DEFAULT=n
    
    Special thanks to h2, author of smxi and inxi, for providing evidence
    that a configuration specific to Debian did not cause stalls found the
    Liquorix kernels under heavy IO load.  This specific configuration
    turned out to be hierarchical scheduling on CFQ (thus, BFQ as well).
    
    4.10:
    During some personal testing with the Dolphin emulator, MuQSS has
    serious problems scaling its frequencies causing poor performance where
    boosting the CPU frequencies would have fixed them.  Reducing the
    up_threshold to 45 with MuQSS appears to fix the issue, letting the
    introduction to "Star Wars: Rogue Leader" run at 100% speed versus about
    80% on my test system.
    
    Also, lets refactor the definitions and include some indentation to help
    the reader discern what the scope of all the macros are.
    
    4.11:
    Increase MICRO_FREQUENCY_UP_THRESHOLD from 95 to 85
    Increase MIN_FREQUENCY_UP_THRESHOLD from 11 to 6
    
    These changes should help make using CFS feel a bit more responsive when
    working with mostly idle workloads, browsing the web, scrolling through
    text, etc.
    
    Increasing the minimum frequency up threshold to 6% may be too
    aggressive though.  Will revert this setting if it causes significant
    battery drain.
    
    4.12:
    Make bfq the default MQ scheduler
    
    Reduce default sampling down factor from 10 to 1
    
    With the world eventually moving to a more laptop heavy configuration,
    it's getting more important that we can reduce our frequencies quickly
    after performing work.  This is normal with a ton of background
    processes that need to perform burst work then sleep.
    
    Since this doesn't really impact performance too much, lets not keep it
    part of ZEN_INTERACTIVE.
    
    Some time ago, the minimum frequency up threshold was set to 1 by
    default, but the zen configuration was never updated to take advantage
    of it.
    
    Remove custom MIN_FREQUENCY_UP_THRESHOLD for MuQSS / ZEN_INTERACTIVE
    configurations and make 1 the default for all choices.
    
    4.18:
    Prefer bfq-mq when available if zen interactive is enabled
    
    The bfq-mq elevator is typically one major kernel version ahead in
    optimizations and bug fixes due to early access patches in the
    algodev/bfq-mq github repository.  Since these patches are typically low
    risk and almost always improve performance and/or increase stability,
    prefer bfq-mq over bfq when available.
    
    Switch from MuQSS to PDS-mq.

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -31,8 +31,13 @@ struct cs_dbs_tuners {
 };
 
 /* Conservative governor macros */
-#define DEF_FREQUENCY_UP_THRESHOLD		(80)
-#define DEF_FREQUENCY_DOWN_THRESHOLD		(20)
+#ifdef CONFIG_SCHED_PDS
+	#define DEF_FREQUENCY_UP_THRESHOLD	(63)
+	#define DEF_FREQUENCY_DOWN_THRESHOLD	(26)
+#else
+	#define DEF_FREQUENCY_UP_THRESHOLD	(80)
+	#define DEF_FREQUENCY_DOWN_THRESHOLD	(20)
+#endif
 #define DEF_FREQUENCY_STEP			(5)
 #define DEF_SAMPLING_DOWN_FACTOR		(1)
 #define MAX_SAMPLING_DOWN_FACTOR		(10)

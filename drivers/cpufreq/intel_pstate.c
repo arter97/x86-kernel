645927459344f52bd41446c58cfdecd2c00deef8
Park Ju Hyung <qkrwngud825@gmail.com>
2019-06-09 23:21:27 +0900

    cpufreq: intel_pstate: adjust EPP value for performance profile
    Speed Shift is extremely fast to respond to load spikes that there's virtually
    no performance difference between EPP 0 and EPP 32, while EPP 32 allows the
    processor to enter the lowest frequency state.
    
    Use EPP 32(0x20) as the new default for the HWP performance profile, and
    introduce a new "full performance" profile for EPP 0.
    
    Sidenote:
    EPP values lower than 120 allows i7-8550U to use boost clocks higher than 3.7 GHz.
    You can test this by using wrmsr without recompiling the kernel:
    For EPP 32:
    rdmsr -a 0x774 (Note the lower 6 digits)
    wrmsr -a 0x774 0x20?????? (Enter the 6 digits from earlier command)
    
    e.g.)
    rdmsr -a 0x774
    80002804
    80002804
    80002804
    80002804
    80002804
    80002804
    80002804
    80002804
    wrmsr -a 0x774 0x20002804
    
    Some people may want to use EPP values like 96(0x60) for "balance_performance",
    but since the power efficiency decreases dramatically beyond 3.7 GHz,
    I'm choosing to leave the default EPP 128 for "balance_performance".
    
    My plan is to use "balance_performance" profile for battery and the new
    "performance" profile on AC.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -660,6 +660,7 @@ static int intel_pstate_set_epb(int cpu, s16 pref)
 
 enum energy_perf_value_index {
 	EPP_INDEX_DEFAULT = 0,
+	EPP_INDEX_FULL_PERFORMANCE,
 	EPP_INDEX_PERFORMANCE,
 	EPP_INDEX_BALANCE_PERFORMANCE,
 	EPP_INDEX_BALANCE_POWERSAVE,
@@ -668,6 +669,7 @@ enum energy_perf_value_index {
 
 static const char * const energy_perf_strings[] = {
 	[EPP_INDEX_DEFAULT] = "default",
+	[EPP_INDEX_FULL_PERFORMANCE] = "full_performance",
 	[EPP_INDEX_PERFORMANCE] = "performance",
 	[EPP_INDEX_BALANCE_PERFORMANCE] = "balance_performance",
 	[EPP_INDEX_BALANCE_POWERSAVE] = "balance_power",
@@ -676,6 +678,7 @@ static const char * const energy_perf_strings[] = {
 };
 static unsigned int epp_values[] = {
 	[EPP_INDEX_DEFAULT] = 0, /* Unused index */
+	[EPP_INDEX_FULL_PERFORMANCE] = HWP_EPP_FULL_PERFORMANCE,
 	[EPP_INDEX_PERFORMANCE] = HWP_EPP_PERFORMANCE,
 	[EPP_INDEX_BALANCE_PERFORMANCE] = HWP_EPP_BALANCE_PERFORMANCE,
 	[EPP_INDEX_BALANCE_POWERSAVE] = HWP_EPP_BALANCE_POWERSAVE,
@@ -693,6 +696,8 @@ static int intel_pstate_get_energy_pref_index(struct cpudata *cpu_data, int *raw
 		return epp;
 
 	if (boot_cpu_has(X86_FEATURE_HWP_EPP)) {
+		if (epp == epp_values[EPP_INDEX_FULL_PERFORMANCE])
+			return EPP_INDEX_FULL_PERFORMANCE;
 		if (epp == epp_values[EPP_INDEX_PERFORMANCE])
 			return EPP_INDEX_PERFORMANCE;
 		if (epp == epp_values[EPP_INDEX_BALANCE_PERFORMANCE])

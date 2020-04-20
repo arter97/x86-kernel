07420a2a80f64390ce2e66f22619c6fd7d360634
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-19 19:59:18 -0700

    mm: Stop kswapd early when nothing's waiting for it to free pages
    Keeping kswapd running when all the failed allocations that invoked it
    are satisfied incurs a high overhead due to unnecessary page eviction
    and writeback, as well as spurious VM pressure events to various
    registered shrinkers. When kswapd doesn't need to work to make an
    allocation succeed anymore, stop it prematurely to save resources.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3742,7 +3742,8 @@ restart:
 		__fs_reclaim_release();
 		ret = try_to_freeze();
 		__fs_reclaim_acquire();
-		if (ret || kthread_should_stop())
+		if (ret || kthread_should_stop() ||
+		    !atomic_read(&pgdat->kswapd_waiters))
 			break;
 
 		/*

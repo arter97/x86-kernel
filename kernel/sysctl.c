abb6c5e8c8460b737e81497a204dda55d61fe1ae
Juhyung Park <qkrwngud825@gmail.com>
2022-07-17 17:26:06 +0900

    Revert "mm: merge Samsung mm hacks"
    This reverts commit ce19e34395284d05a1a0761b6e2f1e1b3b8d39c5.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -132,9 +132,6 @@ static unsigned long one_ul = 1;
 static unsigned long long_max = LONG_MAX;
 static int one_hundred = 100;
 static int one_thousand = 1000;
-#ifdef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
-static int max_swappiness = 200;
-#endif
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
 #endif
@@ -1444,11 +1441,7 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= SYSCTL_ZERO,
-#ifdef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
-		.extra2         = &max_swappiness,
-#else
 		.extra2		= &one_hundred,
-#endif
 	},
 #ifdef CONFIG_HUGETLB_PAGE
 	{

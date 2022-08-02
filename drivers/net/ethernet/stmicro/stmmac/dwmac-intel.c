7d8fc568184270a121d0aa1aab5d2efe706359d7
Song Yoong Siang <yoong.siang.song@intel.com>
2022-08-02 22:57:40 +0800

    net: stmmac: Adjust mac_capabilities for Intel mGbE 2.5G mode
    In the case where kernel driver has no access to modify the clock rate
    after it is increased by 2.5 times in the BIOS to support 2.5G mode,
    link speeds other than 2.5Gbps are not supported. Therefore, this commit
    remove 10 Mbps, 100 Mbps, and 1Gbps support from mac_capabilities list.
    
    Signed-off-by: Tan Tee Min <tee.min.tan@linux.intel.com>
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-intel.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-intel.c
@@ -300,8 +300,10 @@ static void tgl_get_interfaces(struct stmmac_priv *priv, void *bsp_priv,
 		dev_info(priv->device, "Link Speed Mode: 2.5Gbps\n");
 		priv->plat->mdio_bus_data->default_an_inband = false;
 		interface = PHY_INTERFACE_MODE_2500BASEX;
+		priv->plat->fixed_2G5_clock_rate = true;
 	} else {
 		interface = PHY_INTERFACE_MODE_SGMII;
+		priv->plat->fixed_2G5_clock_rate = false;
 	}
 
 	__set_bit(interface, interfaces);

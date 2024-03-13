0f61e02df41b2c251a49dc4b76df2c8619abe873
Juhyung Park <qkrwngud825@gmail.com>
2024-03-13 17:16:51 +0900

    lrng: base backport for v53
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/char/lrng/lrng_drng_chacha20.c
+++ drivers/char/lrng/lrng_drng_chacha20.c
@@ -159,7 +159,7 @@ static void lrng_cc20_drng_dealloc(void *drng)
 	struct chacha20_state *chacha20_state = (struct chacha20_state *)drng;
 
 	pr_debug("ChaCha20 core zeroized and freed\n");
-	kfree_sensitive(chacha20_state);
+	kzfree(chacha20_state);
 }
 
 static const char *lrng_cc20_drng_name(void)
--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7,9 +7,6 @@
  *  Copyright (C) 1991-2002  Linus Torvalds
  */
 #include <linux/lrng.h>
-#include <linux/highmem.h>
-#include <linux/hrtimer_api.h>
-#include <linux/ktime_api.h>
 
 #include "sched.h"
 

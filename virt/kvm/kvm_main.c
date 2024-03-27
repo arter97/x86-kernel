1bc6ea99943e788a767ab4f0ea8d0d113c3de5f6
Yuan Yao <yuan.yao@intel.com>
2024-03-27 13:41:05 +0800

    UBUNTU: SAUCE: KVM: TDX: Return -EINTR for KVM_MEMORY_MAPPING when signal pending
    BugLink: https://bugs.launchpad.net/bugs/2085104
    
    Return -EINTR for signal pending. The -ERESTARTSYS is not
    available to user space for interrupted system call.
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>
    (cherry picked from github.com/intel/kernel-downstream commit f571b5c09e32d9466af592f0eb5e843a3b1ba3ee)
    Signed-off-by: Thibault Ferrante <thibault.ferrante@canonical.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -4480,7 +4480,7 @@ static int kvm_vcpu_memory_mapping(struct kvm_vcpu *vcpu,
 
 	while (mapping->nr_pages) {
 		if (signal_pending(current)) {
-			r = -ERESTARTSYS;
+			r = -EINTR;
 			break;
 		}
 

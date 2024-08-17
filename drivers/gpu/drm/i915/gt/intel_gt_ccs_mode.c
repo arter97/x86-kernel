fffbb43c1dfefebdc71baaf06a32a54cdebbbb67
Andi Shyti <andi.shyti@linux.intel.com>
2024-08-17 23:00:20 +0200

    drm/i915/gt: Remove cslices mask value from the CCS structure
    Following the decision to manage CCS engine creation within UABI
    engines, the "cslices" variable in the "ccs" structure in the
    "gt" is no longer needed. Remove it is now redundant.
    
    Signed-off-by: Andi Shyti <andi.shyti@linux.intel.com>
    (cherry picked from commit 7ba72048794e98bb15380c772f938fc2eafe6736)

--- drivers/gpu/drm/i915/gt/intel_gt_ccs_mode.c
+++ drivers/gpu/drm/i915/gt/intel_gt_ccs_mode.c
@@ -9,7 +9,7 @@
 
 static void intel_gt_apply_ccs_mode(struct intel_gt *gt)
 {
-	unsigned long cslices_mask = gt->ccs.cslices;
+	unsigned long cslices_mask = CCS_MASK(gt);
 	u32 mode_val = 0;
 	/* CCS engine id, i.e. the engines position in the engine's bitmask */
 	int engine;

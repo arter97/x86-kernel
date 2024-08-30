2e32aca49ddcade7b982d60c2be76f9f94b27f70
Isaku Yamahata <isaku.yamahata@intel.com>
2024-08-30 21:07:58 +0800

    UBUNTU: SAUCE: KVM: TDX: Make pmu_intel.c ignore guest TD case
    BugLink: https://bugs.launchpad.net/bugs/2085104
    
    Because TDX KVM doesn't support PMU yet (it's future work of TDX KVM
    support as another patch series) and pmu_intel.c touches vmx specific
    structure in vcpu initialization, as workaround add dummy structure to
    struct vcpu_tdx and pmu_intel.c can ignore TDX case.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    (cherry picked from github.com/intel/kernel-downstream commit 6f7d696f600dbd7dd2b517b20a6e490ffb5742c9)
    Signed-off-by: Thibault Ferrante <thibault.ferrante@canonical.com>

--- arch/x86/kvm/vmx/vmx.h
+++ arch/x86/kvm/vmx/vmx.h
@@ -16,6 +16,7 @@
 #include "../cpuid.h"
 #include "run_flags.h"
 #include "../mmu.h"
+#include "pmu_intel.h"
 
 #define X2APIC_MSR(r) (APIC_BASE_MSR + ((r) >> 4))
 
@@ -88,22 +89,6 @@ union vmx_exit_reason {
 		u32	failed_vmentry		: 1;
 	};
 	u32 full;
-};
-
-struct lbr_desc {
-	/* Basic info about guest LBR records. */
-	struct x86_pmu_lbr records;
-
-	/*
-	 * Emulate LBR feature via passthrough LBR registers when the
-	 * per-vcpu guest LBR event is scheduled on the current pcpu.
-	 *
-	 * The records may be inaccurate if the host reclaims the LBR.
-	 */
-	struct perf_event *event;
-
-	/* True if LBRs are marked as not intercepted in the MSR bitmap */
-	bool msr_passthrough;
 };
 
 extern struct x86_pmu_lbr vmx_lbr_caps;
@@ -658,21 +643,6 @@ static __always_inline struct kvm_vmx *to_kvm_vmx(struct kvm *kvm)
 static __always_inline struct vcpu_vmx *to_vmx(struct kvm_vcpu *vcpu)
 {
 	return container_of(vcpu, struct vcpu_vmx, vcpu);
-}
-
-static inline struct lbr_desc *vcpu_to_lbr_desc(struct kvm_vcpu *vcpu)
-{
-	return &to_vmx(vcpu)->lbr_desc;
-}
-
-static inline struct x86_pmu_lbr *vcpu_to_lbr_records(struct kvm_vcpu *vcpu)
-{
-	return &vcpu_to_lbr_desc(vcpu)->records;
-}
-
-static inline bool intel_pmu_lbr_is_enabled(struct kvm_vcpu *vcpu)
-{
-	return !!vcpu_to_lbr_records(vcpu)->nr;
 }
 
 void intel_pmu_cross_mapped_check(struct kvm_pmu *pmu);

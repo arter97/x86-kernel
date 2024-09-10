cea84de0933baa9b33e673f308fa143c4ad2eb6e
Isaku Yamahata <isaku.yamahata@intel.com>
2024-09-10 10:51:41 +0800

    UBUNTU: SAUCE: KVM: x86/tdp_mmu: Init role member of struct kvm_mmu_page at allocation
    BugLink: https://bugs.launchpad.net/bugs/2085104
    
    Refactor tdp_mmu_alloc_sp() and tdp_mmu_init_sp and eliminate
    tdp_mmu_init_child_sp().  Currently tdp_mmu_init_sp() (or
    tdp_mmu_init_child_sp()) sets kvm_mmu_page.role after tdp_mmu_alloc_sp()
    allocating struct kvm_mmu_page and its page table page.  This patch makes
    tdp_mmu_alloc_sp() initialize kvm_mmu_page.role instead of
    tdp_mmu_init_sp().
    
    To handle private page tables, argument of is_private needs to be passed
    down.  Given that already page level is passed down, it would be cumbersome
    to add one more parameter about sp. Instead replace the level argument with
    union kvm_mmu_page_role.  Thus the number of argument won't be increased
    and more info about sp can be passed down.
    
    For private sp, secure page table will be also allocated in addition to
    struct kvm_mmu_page and page table (spt member).  The allocation functions
    (tdp_mmu_alloc_sp() and __tdp_mmu_alloc_sp_for_split()) need to know if the
    allocation is for the conventional page table or private page table.  Pass
    union kvm_mmu_role to those functions and initialize role member of struct
    kvm_mmu_page.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    (cherry picked from github.com/intel/kernel-downstream commit e8e00f572b9fa6837b9b21bf02e6dda9ede54eda)
    Signed-off-by: Thibault Ferrante <thibault.ferrante@canonical.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -184,43 +184,35 @@ static struct kvm_mmu_page *tdp_mmu_next_root(struct kvm *kvm,
 #define for_each_valid_tdp_mmu_root(_kvm, _root, _as_id)		\
 	__for_each_tdp_mmu_root(_kvm, _root, _as_id, true)
 
-static struct kvm_mmu_page *tdp_mmu_alloc_sp(struct kvm_vcpu *vcpu)
+static struct kvm_mmu_page *tdp_mmu_alloc_sp(struct kvm_vcpu *vcpu,
+					     union kvm_mmu_page_role role)
 {
 	struct kvm_mmu_page *sp;
 
 	sp = kvm_mmu_memory_cache_alloc(&vcpu->arch.mmu_page_header_cache);
 	sp->spt = kvm_mmu_memory_cache_alloc(&vcpu->arch.mmu_shadow_page_cache);
+	sp->role = role;
 
 	return sp;
 }
 
 static void tdp_mmu_init_sp(struct kvm_mmu_page *sp, tdp_ptep_t sptep,
-			    gfn_t gfn, union kvm_mmu_page_role role)
+			    gfn_t gfn)
 {
 	INIT_LIST_HEAD(&sp->possible_nx_huge_page_link);
 
 	set_page_private(virt_to_page(sp->spt), (unsigned long)sp);
 
-	sp->role = role;
+	/*
+	 * role must be set before calling this function.  At least role.level
+	 * is not 0 (PG_LEVEL_NONE).
+	 */
+	WARN_ON_ONCE(!sp->role.word);
 	sp->gfn = gfn;
 	sp->ptep = sptep;
 	sp->tdp_mmu_page = true;
 
 	trace_kvm_mmu_get_page(sp, true);
-}
-
-static void tdp_mmu_init_child_sp(struct kvm_mmu_page *child_sp,
-				  struct tdp_iter *iter)
-{
-	struct kvm_mmu_page *parent_sp;
-	union kvm_mmu_page_role role;
-
-	parent_sp = sptep_to_sp(rcu_dereference(iter->sptep));
-
-	role = parent_sp->role;
-	role.level--;
-
-	tdp_mmu_init_sp(child_sp, iter->sptep, iter->gfn, role);
 }
 
 int kvm_tdp_mmu_alloc_root(struct kvm_vcpu *vcpu)
@@ -260,8 +252,8 @@ int kvm_tdp_mmu_alloc_root(struct kvm_vcpu *vcpu)
 			goto out_spin_unlock;
 	}
 
-	root = tdp_mmu_alloc_sp(vcpu);
-	tdp_mmu_init_sp(root, NULL, 0, role);
+	root = tdp_mmu_alloc_sp(vcpu, role);
+	tdp_mmu_init_sp(root, NULL, 0);
 
 	/*
 	 * TDP MMU roots are kept until they are explicitly invalidated, either
@@ -1140,8 +1132,8 @@ int kvm_tdp_mmu_map(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 		 * The SPTE is either non-present or points to a huge page that
 		 * needs to be split.
 		 */
-		sp = tdp_mmu_alloc_sp(vcpu);
-		tdp_mmu_init_child_sp(sp, &iter);
+		sp = tdp_mmu_alloc_sp(vcpu, tdp_iter_child_role(&iter));
+		tdp_mmu_init_sp(sp, iter.sptep, iter.gfn);
 
 		sp->nx_huge_page_disallowed = fault->huge_page_disallowed;
 
@@ -1451,7 +1443,8 @@ retry:
 			continue;
 		}
 
-		tdp_mmu_init_child_sp(sp, &iter);
+		sp->role = tdp_iter_child_role(&iter);
+		tdp_mmu_init_sp(sp, iter.sptep, iter.gfn);
 
 		if (tdp_mmu_split_huge_page(kvm, &iter, sp, shared))
 			goto retry;

2f4051ef2f946f044624f2c29c463dfa10ba9a2e
Juhyung Park <qkrwngud825@gmail.com>
2025-04-11 01:45:02 -0700

    lz4: adapt to Linux kernel
    A quick benchmark shows this improves zram performance by 3.8% in
    4K blocks, 3.4% in 1M blocks.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -1,1 +1,18 @@
-../../lib/lz4/lz4.h
+/* SPDX-License-Identifier: BSD-2-Clause */
+// LZ4 compatibility wrapper for Linux kernel
+
+#ifndef __LINUX_LZ4_H__
+#define __LINUX_LZ4_H__
+
+#define LZ4_STATIC_LINKING_ONLY
+
+#include "../../lib/lz4/lz4.h"
+#include "../../lib/lz4/lz4hc.h"
+
+#define LZ4_MEM_COMPRESS	LZ4_STREAM_MINSIZE
+#define LZ4HC_MEM_COMPRESS	LZ4_STREAMHC_MINSIZE
+
+#define LZ4HC_MIN_CLEVEL	LZ4HC_CLEVEL_MIN
+#define LZ4HC_MAX_CLEVEL	LZ4HC_CLEVEL_MAX
+
+#endif

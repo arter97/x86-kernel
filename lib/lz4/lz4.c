b912a24775dbb49a497cb51093b862e26ef45d59
Juhyung Park <qkrwngud825@gmail.com>
2025-06-24 14:07:11 -0700

    lz4: fix LZ4_compress_destSize() definition
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- lib/lz4/lz4.c
+++ lib/lz4/lz4.c
@@ -1394,26 +1394,7 @@ int LZ4_compress_destSize_extState(void* state, const char* src, char* dst, int*
     LZ4_initStream(state, sizeof (LZ4_stream_t));
     return r;
 }
-
-
-int LZ4_compress_destSize(const char* src, char* dst, int* srcSizePtr, int targetDstSize)
-{
-#if (LZ4_HEAPMODE)
-    LZ4_stream_t* const ctx = (LZ4_stream_t*)ALLOC(sizeof(LZ4_stream_t));   /* malloc-calloc always properly aligned */
-    if (ctx == NULL) return 0;
-#else
-    LZ4_stream_t ctxBody;
-    LZ4_stream_t* const ctx = &ctxBody;
-#endif
-
-    int result = LZ4_compress_destSize_extState_internal(ctx, src, dst, srcSizePtr, targetDstSize, 1);
-
-#if (LZ4_HEAPMODE)
-    FREEMEM(ctx);
-#endif
-    return result;
-}
-
+EXPORT_SYMBOL(LZ4_compress_destSize_extState);
 
 
 /*-******************************

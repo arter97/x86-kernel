intel/mainline-tracking

drm/i915: SR-IOV Enabling and Support
SR-IOV Enabling and Support. Ported from 6.9 Kernel following
dii-client(6.6) SRIOV changes.

Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
Signed-off-by: Kooran Paul, Princy <princy.kooran.paul@intel.com>

drivers/gpu/drm/i915/i915_irq.c
@@ -46,6 +46,7 @@
 #include "gt/intel_gt_pm_irq.h"
 #include "gt/intel_gt_regs.h"
 #include "gt/intel_rps.h"
+#include "gt/iov/intel_iov_memirq.h"
 
 #include "i915_driver.h"
 #include "i915_drv.h"
@@ -659,6 +660,43 @@ static irqreturn_t dg1_irq_handler(int irq, void *arg)
 	return IRQ_HANDLED;
 }
 
+static irqreturn_t vf_mem_irq_handler(int irq, void *arg)
+{
+	struct drm_i915_private * const i915 = arg;
+	struct intel_gt *gt;
+	unsigned int i;
+
+	if (!intel_irqs_enabled(i915))
+		return IRQ_NONE;
+
+	for_each_gt(gt, i915, i)
+		intel_iov_memirq_handler(&gt->iov);
+
+	pmu_irq_stats(i915, IRQ_HANDLED);
+
+	return IRQ_HANDLED;
+}
+
+static void vf_mem_irq_reset(struct drm_i915_private *i915)
+{
+	struct intel_gt *gt;
+	unsigned int i;
+
+	for_each_gt(gt, i915, i)
+		intel_iov_memirq_reset(&gt->iov);
+}
+
+static int vf_mem_irq_postinstall(struct drm_i915_private *i915)
+{
+	struct intel_gt *gt;
+	unsigned int i;
+
+	for_each_gt(gt, i915, i)
+		intel_iov_memirq_postinstall(&gt->iov);
+
+	return 0;
+}
+
 static void ibx_irq_reset(struct drm_i915_private *dev_priv)
 {
 	struct intel_uncore *uncore = &dev_priv->uncore;
@@ -732,8 +770,10 @@ static void gen11_irq_reset(struct drm_i915_private *dev_priv)
 	gen11_gt_irq_reset(gt);
 	gen11_display_irq_reset(dev_priv);
 
-	GEN3_IRQ_RESET(uncore, GEN11_GU_MISC_);
-	GEN3_IRQ_RESET(uncore, GEN8_PCU_);
+	if (!IS_SRIOV_VF(dev_priv)) {
+		GEN3_IRQ_RESET(uncore, GEN11_GU_MISC_);
+		GEN3_IRQ_RESET(uncore, GEN8_PCU_);
+	}
 }
 
 static void dg1_irq_reset(struct drm_i915_private *dev_priv)
@@ -809,7 +849,8 @@ static void gen11_irq_postinstall(struct drm_i915_private *dev_priv)
 	gen11_gt_irq_postinstall(gt);
 	gen11_de_irq_postinstall(dev_priv);
 
-	GEN3_IRQ_INIT(uncore, GEN11_GU_MISC_, ~gu_misc_masked, gu_misc_masked);
+	if (!IS_SRIOV_VF(dev_priv))
+		GEN3_IRQ_INIT(uncore, GEN11_GU_MISC_, ~gu_misc_masked, gu_misc_masked);
 
 	gen11_master_intr_enable(intel_uncore_regs(uncore));
 	intel_uncore_posting_read(&dev_priv->uncore, GEN11_GFX_MSTR_IRQ);
@@ -1323,7 +1364,9 @@ static irq_handler_t intel_irq_handler(struct drm_i915_private *dev_priv)
 		else
 			return i8xx_irq_handler;
 	} else {
-		if (GRAPHICS_VER_FULL(dev_priv) >= IP_VER(12, 10))
+		if (HAS_MEMORY_IRQ_STATUS(dev_priv))
+			return vf_mem_irq_handler;
+		else if (GRAPHICS_VER_FULL(dev_priv) >= IP_VER(12, 10))
 			return dg1_irq_handler;
 		else if (GRAPHICS_VER(dev_priv) >= 11)
 			return gen11_irq_handler;
@@ -1348,7 +1391,9 @@ static void intel_irq_reset(struct drm_i915_private *dev_priv)
 		else
 			i8xx_irq_reset(dev_priv);
 	} else {
-		if (GRAPHICS_VER_FULL(dev_priv) >= IP_VER(12, 10))
+		if (HAS_MEMORY_IRQ_STATUS(dev_priv))
+			vf_mem_irq_reset(dev_priv);
+		else if (GRAPHICS_VER_FULL(dev_priv) >= IP_VER(12, 10))
 			dg1_irq_reset(dev_priv);
 		else if (GRAPHICS_VER(dev_priv) >= 11)
 			gen11_irq_reset(dev_priv);
@@ -1373,7 +1418,9 @@ static void intel_irq_postinstall(struct drm_i915_private *dev_priv)
 		else
 			i8xx_irq_postinstall(dev_priv);
 	} else {
-		if (GRAPHICS_VER_FULL(dev_priv) >= IP_VER(12, 10))
+		if (HAS_MEMORY_IRQ_STATUS(dev_priv))
+			vf_mem_irq_postinstall(dev_priv);
+		else if (GRAPHICS_VER_FULL(dev_priv) >= IP_VER(12, 10))
 			dg1_irq_postinstall(dev_priv);
 		else if (GRAPHICS_VER(dev_priv) >= 11)
 			gen11_irq_postinstall(dev_priv);

arter97/x86-kernel

Signed-off-by: Zawawi, Muhammad Zul Husni <muhammad.zul.husni.zawawi@intel.com>
(cherry picked from commit a3715502dea38ba15fa19643d19d3488c5684988)

drivers/gpu/drm/i915/i915_irq.c
@@ -46,6 +46,7 @@
 #include "gt/intel_gt_pm_irq.h"
 #include "gt/intel_gt_regs.h"
 #include "gt/intel_rps.h"
+#include "gt/iov/intel_iov_memirq.h"
 
 #include "i915_driver.h"
 #include "i915_drv.h"
@@ -653,6 +654,43 @@ static irqreturn_t dg1_irq_handler(int irq, void *arg)
 	return IRQ_HANDLED;
 }
 
+static irqreturn_t vf_mem_irq_handler(int irq, void *arg)
+{
+	struct drm_i915_private * const i915 = arg;
+	struct intel_gt *gt;
+	unsigned int i;
+
+	if (!intel_irqs_enabled(i915))
+		return IRQ_NONE;
+
+	for_each_gt(gt, i915, i)
+		intel_iov_memirq_handler(&gt->iov);
+
+	pmu_irq_stats(i915, IRQ_HANDLED);
+
+	return IRQ_HANDLED;
+}
+
+static void vf_mem_irq_reset(struct drm_i915_private *i915)
+{
+	struct intel_gt *gt;
+	unsigned int i;
+
+	for_each_gt(gt, i915, i)
+		intel_iov_memirq_reset(&gt->iov);
+}
+
+static int vf_mem_irq_postinstall(struct drm_i915_private *i915)
+{
+	struct intel_gt *gt;
+	unsigned int i;
+
+	for_each_gt(gt, i915, i)
+		intel_iov_memirq_postinstall(&gt->iov);
+
+	return 0;
+}
+
 static void ilk_irq_reset(struct drm_i915_private *dev_priv)
 {
 	struct intel_display *display = dev_priv->display;
@@ -709,8 +747,10 @@ static void gen11_irq_reset(struct drm_i915_private *dev_priv)
 	gen11_gt_irq_reset(gt);
 	gen11_display_irq_reset(display);
 
-	gen2_irq_reset(uncore, GEN11_GU_MISC_IRQ_REGS);
-	gen2_irq_reset(uncore, GEN8_PCU_IRQ_REGS);
+	if (!IS_SRIOV_VF(dev_priv)) {
+		gen2_irq_reset(uncore, GEN11_GU_MISC_IRQ_REGS);
+		gen2_irq_reset(uncore, GEN8_PCU_IRQ_REGS);
+	}
 }
 
 static void dg1_irq_reset(struct drm_i915_private *dev_priv)
@@ -789,7 +829,8 @@ static void gen11_irq_postinstall(struct drm_i915_private *dev_priv)
 	gen11_gt_irq_postinstall(gt);
 	gen11_de_irq_postinstall(display);
 
-	gen2_irq_init(uncore, GEN11_GU_MISC_IRQ_REGS, ~gu_misc_masked, gu_misc_masked);
+	if (!IS_SRIOV_VF(dev_priv))
+		gen2_irq_init(uncore, GEN11_GU_MISC_IRQ_REGS, ~gu_misc_masked, gu_misc_masked);
 
 	gen11_master_intr_enable(intel_uncore_regs(uncore));
 	intel_uncore_posting_read(&dev_priv->uncore, GEN11_GFX_MSTR_IRQ);
@@ -1148,7 +1189,9 @@ void intel_irq_fini(struct drm_i915_private *i915)
 
 static irq_handler_t intel_irq_handler(struct drm_i915_private *dev_priv)
 {
-	if (GRAPHICS_VER_FULL(dev_priv) >= IP_VER(12, 10))
+	if (HAS_MEMORY_IRQ_STATUS(dev_priv))
+		return vf_mem_irq_handler;
+	else if (GRAPHICS_VER_FULL(dev_priv) >= IP_VER(12, 10))
 		return dg1_irq_handler;
 	else if (GRAPHICS_VER(dev_priv) >= 11)
 		return gen11_irq_handler;
@@ -1168,7 +1211,9 @@ static irq_handler_t intel_irq_handler(struct drm_i915_private *dev_priv)
 
 static void intel_irq_reset(struct drm_i915_private *dev_priv)
 {
-	if (GRAPHICS_VER_FULL(dev_priv) >= IP_VER(12, 10))
+	if (HAS_MEMORY_IRQ_STATUS(dev_priv))
+		vf_mem_irq_reset(dev_priv);
+	else if (GRAPHICS_VER_FULL(dev_priv) >= IP_VER(12, 10))
 		dg1_irq_reset(dev_priv);
 	else if (GRAPHICS_VER(dev_priv) >= 11)
 		gen11_irq_reset(dev_priv);
@@ -1188,7 +1233,9 @@ static void intel_irq_reset(struct drm_i915_private *dev_priv)
 
 static void intel_irq_postinstall(struct drm_i915_private *dev_priv)
 {
-	if (GRAPHICS_VER_FULL(dev_priv) >= IP_VER(12, 10))
+	if (HAS_MEMORY_IRQ_STATUS(dev_priv))
+		vf_mem_irq_postinstall(dev_priv);
+	else if (GRAPHICS_VER_FULL(dev_priv) >= IP_VER(12, 10))
 		dg1_irq_postinstall(dev_priv);
 	else if (GRAPHICS_VER(dev_priv) >= 11)
 		gen11_irq_postinstall(dev_priv);